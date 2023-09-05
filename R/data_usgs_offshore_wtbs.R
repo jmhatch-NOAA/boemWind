d98748c4ee3334162165b2ffef29530cf3795d0d
jmhatch <jmhatch@vt.edu>
2023-09-05 12:39:58 -0400

    Add wind turbine locations

---
+++ R/data_usgs_offshore_wtbs.R
@@ -0,0 +1,20 @@
+#' @title The United States Wind Turbine Database (USWTDB)
+#' @details Version: 6.0.0 - 2023-5-31
+#'
+#' @description An \code{sf} object containing the locations for offshore wind turbines.
+#'
+#' @format An \code{sf} collection with 7 features and 28 fields.
+#' \describe{
+#'   \item{Geometry type}{POINT}
+#'   \item{Dimension}{XY}
+#'   \item{Bounding box}{xmin: -75.4916 ymin: 36.8868 xmax: -71.5076 ymax: 41.1257}
+#'   \item{Geodetic CRS}{NAD83}
+#'   \item{Source}{data-raw/usgs-uswtdb-shapefile/uswtdb_v6_0_20230531.shp}
+#' }
+#'
+#' @docType data
+#' @name usgs_offshore_wtbs
+#' @usage data('usgs_offshore_wtbs')
+#' @keywords datasets
+#' @source \url{https://eerscmap.usgs.gov/uswtdb/}
+NULL
